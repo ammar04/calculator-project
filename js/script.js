b830804e990faacd7f0cbf8d0779fbefb394811d
Your Name <you@example.com>
2018-07-05 11:23:41 +0300

    modified js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var add=function(number1, number2) {return number1+number2};
+var number1=parseFloat(prompt("enter a number"));
+var number2=parseFloat(prompt("enter another number"));
+alert(add(number1, number2));
